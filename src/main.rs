291fd2145000110be558559975171d055346d513
Lenka2136 <69755189+Lenka2136@users.noreply.github.com>
2024-05-17 20:13:18 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs};
+use logos::Logos;
+use c1::lexer::Token;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let path = match args.get(1) {
+        None => "tests/inputs/ok/demorgan.c1",
+        Some(path) => path
+    };
+    let input = fs::read_to_string(path).expect("input file should exist");
+    let mut lexer = Token::lexer(input.as_str());
+
+    while let Some(val) = lexer.next() {
+        println!("{:?}: {:?}", val, lexer.slice());
+    }
 }
